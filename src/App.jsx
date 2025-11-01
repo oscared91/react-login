e4e7eaa4a3181ccabbc4abefda2d9b39a9b54180
ACER <oscar.morenopre@gmail.com>
2025-11-01 07:40:18 -0500

    chore(app.jsx): login app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react"
+import {
+  MDBContainer,
+  MDBCol, 
+  MDBRow, 
+  MDBBtn, 
+  MDBIcon, 
+  MDBInput, 
+  MDBCheckbox,
+  MDBCard 
+            } from 'mdb-react-ui-kit'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App () {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <MDBContainer fluid>
+      <MDBRow>
+
+        <MDBCol xl='6' col="10" className=" d-flex">
+          <div className="d-flex flex-row ps-5 pt-5">
+            <img src="gato.png" className="img-fluid" style={{width:120, height:120}} />
+            <span className="h2 fw-bold mb-2"> GD: Gato_Dev </span>
+          </div>
+
+          <div className="d-flex col-10 flex-column justify-content-center h-custom-2 w-75 pt-4">
+            <h3 className="fw-normal mb-3 ps-5 pb-3" style={{letterSpacing: '1px'}}>Log in</h3>
+            <MDBInput 
+                wrapperClass = 'mb-4 ' 
+                label='Email address' 
+                id='formControlLg-email' 
+                type='email' 
+                size='lg'/>
+            <MDBInput 
+                wrapperClass = 'mb-4' 
+                label='Password' 
+                id='formControlLg-password' 
+                type='password' 
+                size='lg'/> 
+            <div className="d-flex justify-content-between mx-1 mb-4">
+                <MDBCheckbox name='flexCheck' value='' id='flexCheckDefaul' label='Remember me' />
+                <a href=''>Forgot password</a> 
+            </div>
+            <MDBBtn className="col-10 justify-content-center mb-0 px-4" size='lg'>Login</MDBBtn>
+            <p className="small fw bold mt-2 pt-1 mb-2">Don't have an account? <a href='' className="link-danger">Register</a></p>
+            <div className="divider col-10 align-items-center my-4">
+              <p className="text-center fw-bold mx-3 mb-0">OR</p>
+            </div>
+            <MDBBtn className="col-10 mb-4 " size="lg" style={{backgroundColor: '#3b5998'}}>
+            <MDBIcon fab icon="google" className="mx-2" />
+            Continue with google
+          </MDBBtn>
+          </div>
+        </MDBCol>
+        
+        <MDBCol xl="6" className='d-None d-sm-block px-0'>
+          <img  src ="tuli.jpg" 
+            className="w-100" 
+            alt="Login image"
+            style={{objectFit: 'cover', objectPosition: 'left'}}/>
+        </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+      )
+    }
 
 export default App
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAgUgxYLJYq2dRodwOEDpS_hzw-mcf1nYQ",
+  authDomain: "react-login-36c24.firebaseapp.com",
+  projectId: "react-login-36c24",
+  storageBucket: "react-login-36c24.firebasestorage.app",
+  messagingSenderId: "907799922147",
+  appId: "1:907799922147:web:e50b63cd661e012e098cd6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
