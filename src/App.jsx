0d0a473df971b91e591f44861ddc62d8ea2d2fcb
ACER <oscar.morenopre@gmail.com>
2025-11-01 19:57:42 -0500

    chore(app.jsx): validación de campos

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,24 @@ import {
   MDBTabsItem,
   MDBTabsLink,
   MDBTabsContent,
-  MDBTabsPane
+  MDBTabsPane,
+  MDBValidation,
+  MDBValidationItem
             } from 'mdb-react-ui-kit'
 
 function App () {
   
   const [justifyActive, setJustifyActive] = useState('tab1')
-  
+  const [formValueLogin, setFormValueLogin] = useState({
+    email: '',
+    password: ''
+  })
+  const [formValueRegister, setFormValueRegister] = useState({
+    email: '',
+    username: '',
+    password: '',
+
+  })
   const handleJustifyClick = (value) => {
     if (value === justifyActive) {
       return
@@ -27,16 +38,23 @@ function App () {
     setJustifyActive(value)
   }
 
+  const onChangeLogin = (e) => {
+    setFormValueLogin({...formValueLogin,[e.target.name]: e.target.value})
+  }
+  const onChangeRagister = (e) => {
+    setFormValueRegister({...formValueRegister, [e.target.name]: e.target.value})
+  }
+
   return (
     <MDBContainer fluid className="p-3 my-5 d-flex flex-column w-50">
           <div className="d-flex aling-items-center justify-content-center ps-5 pt-5 pb-5">
             <img src="gato.png" className="img-fluid" style={{width:90, height:90}} />
             <span className="h3 fw-bold mb-2"> GD: Gato_Dev </span>
           </div>
-      <MDBRow>
+      <MDBRow className="d-flex">
         <MDBCol xl='6' col="10" className=" ">
       
-        <MDBTabs pills justify className="mb-3 d-flex flex-row justify-content-between">
+        <MDBTabs fill justify className="mb-3 d-flex flex-row justify-content-between">
           <MDBTabsItem>
             <MDBTabsLink onClick={() => handleJustifyClick('tab1')} active={justifyActive === 'tab1'}>
               Login
@@ -54,7 +72,7 @@ function App () {
           
           
 {/* sing in google*/}
-      <div className="text-center mb-3">
+      <div className="text-start mb-2">
         <p>Sign in with:</p>
         
         <MDBBtn tag ='a' color='none' className="m-1 " size="lg" style={{color: '#1266f1'}}>
@@ -63,78 +81,110 @@ function App () {
         </MDBBtn>
 {/* or division*/}
       </div>
-      <div className="divider col-10 align-items-center my-4">
-        <p className="text-center fw-bold mx-3 mb-0">OR</p>
+      <div className="divider col-10 align-items-center">
+        <p className="text-center fw-bold mx-5 mb-0">Or</p>
       </div>
 {/*data login*/} 
-      <div className="d-flex col-10 flex-column justify-content-center h-custom-2 w-75 pt-4">
-        <h3 className="fw-normal mb-3 ps-5 pb-3" style={{letterSpacing: '1px'}}>Log in</h3>
+      <div className=" flex-column justify-content-center h-custom-2 w-75 pt-2">
+        <h3 className="fw-normal mb-2 ps-2 pb-3" style={{letterSpacing: '1px'}}>Log in</h3>
+        <MDBValidation>
+        <MDBValidationItem  feedback = "campo requerido" invalid>
+
         <MDBInput 
-            wrapperClass = 'mb-4 ' 
-            label='Email address' 
+            value={formValueLogin.email}
+            name = 'email'
+            wrapperClass = 'mb-4 '
+            onChange={onChangeLogin} 
             id='formControlLg-email' 
+            required
             type='email' 
-            size='lg'/>
+            label='Email address' 
+            size='sg'/>
+        </MDBValidationItem>
+        <MDBValidationItem  feedback = "campo requerido" invalid>
         <MDBInput 
-            wrapperClass = 'mb-4' 
+            value = {formValueLogin.password}
+            name = 'password'
+            onChange={onChangeLogin}
+            wrapperClass = 'mb-4'
+            required 
             label='Password' 
             id='formControlLg-password' 
             type='password' 
-            size='lg'/> 
-        <div className="d-flex justify-content-between mx-1 mb-4">
+            size='sg'/> 
+        </MDBValidationItem>
+        <div className="small d-flex justify-content-between mx-1 mb-4">
             <MDBCheckbox name='flexCheck' value='' id='flexCheckDefaul' label='Remember me' />
             <a href=''>Forgot password</a> 
         </div>
         <MDBBtn className="col-10 justify-content-center mb-0 px-4" size='lg'>Login</MDBBtn>
-        <p className="small fw-bold mt-2 pt-1 mb-2">Don't have an account? <a href='' className="link-danger">Register</a></p>
+        </MDBValidation> 
       </div>
   
     </MDBTabsPane>
 
     
     <MDBTabsPane open = {justifyActive === 'tab2'}>
-      <div className="text-center mb-3">
+      <div className="text-start mb-1">
         <p>Sign in with:</p>
         
+        <MDBIcon fab icon="google" size='6x' />
         <MDBBtn tag ='a' color='none' className="m-1 " size="lg" style={{color: '#1266f1'}}>
-            <MDBIcon fab icon="google" className="sm" />
             
         </MDBBtn>
       </div>
-      <div className="divider col-10 align-items-center my-4">
-        <p className="text-center fw-bold mx-3 mb-0">OR</p>
+      <div className="divider col-10 align-items-center my-1">
+        <p className="text-center fw-bold mx-3 mb-0">Or</p>
       </div>
-      <div className="d-flex col-10 flex-column justify-content-center h-custom-2 w-75 pt-4">
+      <div className="d-flex col-10 flex-column justify-content-center h-custom-2 w-75 pt-2">
         <h3 className="fw-normal mb-3 ps-5 pb-3" style={{letterSpacing: '1px'}}>Log in</h3>
+        <MDBValidation>
+        <MDBValidationItem  className="fw-6" feedback = "campo requerido" invalid>
         <MDBInput 
+            value={formValueRegister.email}
+            name = 'email'
+            onChange={onChangeRagister}
+            required
             wrapperClass = 'mb-4 ' 
-            label='Name' 
+            label='email' 
             id='form1' 
-            type='text' 
+            type='email' 
             size='lg'/>
+        </MDBValidationItem>
+        
+        <MDBValidationItem small feedback = "campo requerido" invalid>
         <MDBInput 
+            value={formValueRegister.username}
+            name="username"
+            onChange={onChangeRagister}
+            required
             wrapperClass = 'mb-4' 
             label='Username' 
             id='form2' 
             type='text' 
             size='lg'/>
+        </MDBValidationItem>
+
+        <MDBValidationItem  feedback = "campo requerido" invalid>
+
         <MDBInput 
-            wrapperClass = 'mb-4' 
-            label='email' 
-            id='form3' 
-            type='email' 
-            size='lg'/>
-        <MDBInput 
+            value={formValueRegister.password}
+            name="password"
+            onChange={onChangeRagister}
+            required
             wrapperClass = 'mb-4' 
             label='Password' 
             id='form4' 
             type='password' 
-            size='lg'/>
-          <div className="d-flex justify-content-center mb-4">
-            <MDBCheckbox name='FlexCheck' id= 'flexCheckDefault' label='he leido todos los terminos y condiciones'/>
+            size="lg"/>
+        </MDBValidationItem>
+          
+          <div className="small d-flex justify-content-center fs-6 mt-3 mb-2">
+            <MDBCheckbox className=""name='FlexCheck' id= 'flexCheckDefault' label='he leido todos los terminos y condiciones' required/>
           </div>
-          <MDBBtn className="mb-4 w-100">Sing up</MDBBtn>  
+          <MDBBtn type="submit" className="mb-4 w-100">Sing up</MDBBtn>  
 
+        </MDBValidation>
         </div>
 
       </MDBTabsPane>
