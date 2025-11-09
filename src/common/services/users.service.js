90fb3c645c3d3094b2605a42d1777eac2b2ed0bd
JohanFZ10 <andresjfz10@gmail.com>
2025-11-09 11:05:48 -0500

    feat(service): create the ´user.service´ file

---
+++ src/common/services/users.service.js
@@ -0,0 +1,10 @@
+// logica asociada a la base de datos y al backend
+const getUserById = () => {
+    // logic
+};
+const getUserByName = () => {
+    // logic
+};
+const getUsers = () => {
+    // logic
+};
